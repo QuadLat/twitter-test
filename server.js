5818250757ddb6828a4acbe0a24f328bdba2a850
KarmaDoer <jatin@quadlat.com>
2016-05-26 04:45:19 -0400

    first commit of code

---
+++ server.js
@@ -0,0 +1,23 @@
+var	morgan = require('morgan'),
+	http = require('http'),
+	finalhandler = require('finalhandler'),
+	fs = require('fs');
+
+//create access log stream for writing to file
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+//add stream to morgan, and morgan to connect
+var logger = morgan('combined', {stream:accessLogStream});
+
+var dispatcher = require(__dirname + '/lib/dispatcher.js');
+
+var server = http.createServer(function(req, res) {
+	var done = finalhandler(req, res)
+	logger(req, res, function(err) {
+		if (err) return done(err);
+		dispatcher.dispatch(req, res);
+	})
+});
+
+server.listen(3030, function() {
+	console.log('Server listening on port 3030');
+});
